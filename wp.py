1cab2e9dfa93b6c16896173ae8432068f7cb46a4
AkashVD <akashdeshpande54@gmail.com>
2020-04-29 14:21:59 +0530

    Checking proper Inputs

---
+++ wp.py
@@ -0,0 +1,9 @@
+#Application for Workout
+print("WORKOUT PLANNER".center(21,"*"))
+
+age = int(input("Enter your age : "))
+weight = float(input("Enter your weight(kg) : "))
+height = float(input("Enter your height(cm) : "))
+
+
+
